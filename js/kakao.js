8f1393c7e0629d40b897472f90af610d6600f52a
tubi55 <tubi55@nate.com>
2024-09-27 19:04:43 +0900

    교통량 보임, 숨김처리

--- js/kakao.js
+++ js/kakao.js
@@ -1,5 +1,6 @@
+const toggleTraffic = document.querySelector(".toggleTraffic");
 const mapContainer = document.querySelector("#map");
-const mapOption = { center: new kakao.maps.LatLng(37.51936795597721, 127.05708259472347), level: 2 };
+const mapOption = { center: new kakao.maps.LatLng(37.51936795597721, 127.05708259472347), level: 4 };
 
 //map instance
 const map = new kakao.maps.Map(mapContainer, mapOption);
@@ -7,5 +8,27 @@ const map = new kakao.maps.Map(mapContainer, mapOption);
 //marker instance
 const marker = new kakao.maps.Marker({ position: mapOption.center });
 
+//type controller instance
+const mapTypeControl = new kakao.maps.MapTypeControl();
+
+//zoom controller instance
+const zoomControl = new kakao.maps.ZoomControl();
+
 //add marker layer on map layer
 marker.setMap(map);
+
+//add controller layer on map layer
+[mapTypeControl, zoomControl].forEach(el => map.addControl(el));
+
+//toggleTraffic Event bind
+toggleTraffic.addEventListener("click", e => {
+	e.target.classList.toggle("on");
+
+	if (e.target.classList.contains("on")) {
+		e.target.inerText = "Traffic OFF";
+		map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	} else {
+		e.target.inerText = "Traffic ON";
+		map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	}
+});
